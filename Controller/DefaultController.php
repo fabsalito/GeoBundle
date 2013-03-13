f4e7fd555da5694b29a4b9fb32f4af9d48897b70
fabsal <fabricio.salinas@gmail.com>
2013-03-13 17:26:58 -0600

    Begin development to basis CRUD actions for service.-

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,12 +2,64 @@
 
 namespace fabsalito\GeoBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+//class DefaultController extends Controller
+class DefaultController
 {
-    public function indexAction($name)
+    /**
+     * Get cities
+     *
+     * @param integer $country_id
+     * @param integer $state_id
+     * @return array
+     */
+    public function getCities($country_id, $state_id)
     {
-        return $this->render('fabsalitoGeoBundle:Default:index.html.twig', array('name' => $name));
+
     }
+
+    /**
+     * Get a city
+     *
+     * @param integer $city_id
+     * @return City
+     */
+    public function getCity($city_id)
+    {
+
+    }
+
+    /**
+     * Add a city
+     *
+     * @param City $city
+     * @return n/a
+     */
+     public function insertCity($city)
+     {
+
+     }
+
+     /**
+      * Set a city
+      *
+      * @param City $city
+      * @return n/a
+      */
+     public function setCity($city)
+     {
+
+     }
+
+     /**
+      * Delete a city
+      *
+      * @param integer $city_id
+      * @return n/a
+      */
+     public function deleteCity($city_id)
+     {
+
+     }
 }
